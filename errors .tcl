040ba71578bbf93c2b44ff031932fc52c88a0a3a
harshith paritala <harshith3805@gmail.com>
2025-07-17 15:56:52 +0530

    Delete errors .tcl

--- errors .tcl
+++
@@ -1,6 +0,0 @@
-cat /openLANE_flow/designs/full_adder/runs/08-07_10-33/logs/floorplan/10-pdn.log | tail -n 30
-
-
-magic -T $PDK_ROOT/sky130A/libs.tech/magic/sky130A.tech \
-  lef read runs/full_adder/results/floorplan/merged.lef \
-  def read runs/full_adder/results/floorplan/full_adder.floorplan.def &
